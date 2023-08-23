ae279f023df6e4c0022a378a2c6908492e932a8b
sletmoe <sletmoe@amazon.com>
2023-08-22 21:38:32 -0700

    Initial implementation

---
+++ lua/plugins/core.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "kaicataldo/material.vim",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "material",
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      theme = "material",
+    },
+  },
+  {
+    "cappyzawa/trim.nvim",
+    opts = {},
+  },
+  {
+    "tpope/vim-sleuth",
+  },
+}
