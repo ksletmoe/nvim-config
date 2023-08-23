295664b985a335c4cbb8586592de0f5990bcd750
sletmoe <sletmoe@amazon.com>
2023-08-23 00:00:15 -0700

    Update nvim theme

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,17 +1,49 @@
 return {
   {
-    "kaicataldo/material.vim",
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      theme = "auto",
+    },
+  },
+  {
+    "marko-cerovac/material.nvim",
+    opts = {
+      material_style = "ocean",
+      lualine_style = "standard",
+      contrast = {
+        terminal = true, -- Enable contrast for the built-in terminal
+        sidebars = true, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+        floating_windows = true, -- Enable contrast for floating windows
+        cursor_line = true, -- Enable darker background for the cursor line
+        non_current_windows = false, -- Enable contrasted background for non-current windows
+        filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
+      },
+      plugins = { -- Uncomment the plugins that you use to highlight them
+        -- Available plugins:
+        -- "dap",
+        -- "dashboard",
+        "gitsigns",
+        -- "hop",
+        -- "indent-blankline",
+        -- "lspsaga",
+        "mini",
+        -- "neogit",
+        -- "neorg",
+        "nvim-cmp",
+        -- "nvim-navic",
+        "nvim-tree",
+        "nvim-web-devicons",
+        -- "sneak",
+        "telescope",
+        "trouble",
+        "which-key",
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "material",
-    },
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = {
-      theme = "material",
     },
   },
   {
