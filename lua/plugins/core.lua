554cdbff5d8d6703a3a4d7fee4bcdde5e8ad3edf
sletmoe <sletmoe@amazon.com>
2023-08-22 21:54:20 -0700

    Tweak a few settings

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -21,4 +21,14 @@ return {
   {
     "tpope/vim-sleuth",
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    },
+  },
 }
