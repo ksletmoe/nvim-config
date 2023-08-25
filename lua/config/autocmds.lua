438e073ca5826d52f34da201e3576f3fb39d5b81
sletmoe <sletmoe@amazon.com>
2023-08-24 21:29:10 -0700

    Restore cursor on exit

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,22 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+function nvim_create_augroups(definitions)
+  for group_name, definition in pairs(definitions) do
+    vim.api.nvim_command("augroup " .. group_name)
+    vim.api.nvim_command("autocmd!")
+    for _, def in ipairs(definition) do
+      local command = table.concat(vim.tbl_flatten({ "autocmd", def }), " ")
+      vim.api.nvim_command(command)
+    end
+    vim.api.nvim_command("augroup END")
+  end
+end
+
+local autocmds = {
+  restore_cursor = {
+    { "VimLeave", "*", "set guicursor=a:ver1-blinkon1" },
+  },
+}
+
+nvim_create_augroups(autocmds)
